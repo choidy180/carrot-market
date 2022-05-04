46438a03893fb95b84b0acc1c255f7624de390d8
choidy180 <choidy180@naver.com>
2022-05-04 19:01:10 +0900

    commit

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,10 +1,17 @@
+import twilio from"twilio";
 import client from "@libs/client/client";
-import withHandler from "@libs/server/withHandler";
+import withHandler, { ResponseType } from "@libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
 
-async function handler(req: NextApiRequest, res: NextApiResponse){
+const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
+
+async function handler(
+  req: NextApiRequest, 
+  res: NextApiResponse<ResponseType>
+) {
   const {email, phone} = req.body;
-  const user = phone ? {phone: +phone} : {email};
+  const user = phone ? {phone: +phone} : email ? {email} : null;
+  if(!user) return res.status(400).json({ok: false});
   const payload = Math.floor(100000 + Math.random() * 900000) + "";
   const token = await client.token.create({
     data: {
@@ -22,8 +29,17 @@ async function handler(req: NextApiRequest, res: NextApiResponse){
       }
     }
   });
-  console.log(token);
-  return res.status(200).end();
+  if(phone){
+    const message = twilioClient.messages.create({
+      messagingServiceSid: process.env.TWILIO_MSID,
+      to: process.env.MY_PHONE!,
+      body: `Your Login Token is ${payload}`,
+    });
+    console.log(twilioClient);
+  }
+  return res.json({
+    ok: true,
+  })
 }
 
 export default withHandler("POST", handler);
