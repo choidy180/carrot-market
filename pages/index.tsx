44cdf0689dd49186829cccc51f807da04b97ce56
choidy180 <choidy180@naver.com>
2022-05-04 15:10:44 +0900

    commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from "next";
-import Layout from "../components/layout";
-import "../libs/client/client";
+import Layout from "@components/layout";
+import "@libs/client/client";
  const Home: NextPage = () => {
    return (
      <Layout title="HOME" hasTabBar>
